35488bf2656d6449f5c097cad5861703b31dd2dd
Adenike Awofeso <adenikeawofeso@gmail.com>
2019-04-22 07:05:31 +0100

    Commit message

--- clock.js
+++ clock.js
@@ -1,3 +1,5 @@
+const apiKey = 'a4ad750391482adc3a14e9858548191e';
+const timeKey ='iC2XDSjHwMgGRsEYGApjYSqYHacCWU';
 const city = document.getElementById('searchText');
 const searchBtn =document.getElementById('searchBtn');
 const searchText = document.getElementById('searchText');
@@ -23,11 +25,20 @@ function findWeather(){
     if (searchText.value === ''){
         alert('Enter a city');
     } else {
+       var searchLink = "api.openweathermap.org/data/2.5/weather?q=" + searchText.value + "&appid=" + apiKey;
+       httpRequestAsync(searchLink, weatherResponse);
 
     }
+function weatherResponse(response){
+    let jsonObject = JSON.parse.response;
+    location.innerHTML = jsonObject.name;
+    weather.innerHTML = parseInt(jsonObject.main.temp -273) +"°";
+}
+
     
 }
 
 function findTime(){
+    var timelink = "https://www.amdoren.com/api/timezone.js?" + searchText.value + "&timeid=" + timeKey;
     
 }
